3a509bf3e67037852b8adb70499a4d68cb64de55
Hordienkoihor <hordienkoihor05@gmail.com>
2026-06-08 02:36:35 +0300

    chore: add bg

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,11 +1,16 @@
 package game;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
+
+    public static Stage primaryStage;
 
     public static void main(String[] args){
         launch(args);
@@ -13,8 +18,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root);
+        Main.primaryStage = stage;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 900, 500);
+
         stage.setTitle("Test window");
         stage.setScene(scene);
         stage.show();
