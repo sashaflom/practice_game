fbccbbc278a6857b831eb3465ad399533acbe504
Oleksandra Flomboim <sflomboim@gmail.com>
2026-06-16 04:31:39 +0300

    delete some commented code

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -2,14 +2,9 @@ package game;
 
 import game.menu.start.MainMenu;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
@@ -25,9 +20,6 @@ public class Main extends Application {
         stage.setTitle("Jungle Dash: Kong's Record");
         stage.setScene(scene);
 
-//        GameLoop timer = new GameLoop();
-//        GameService.setUp(stage, root);
-//        timer.start();
         MainMenu mainMenu = new MainMenu(
                 "/images/main_menu_bg.png",
                 "/images/main_menu_start_btn.png",
